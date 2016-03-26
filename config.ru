272a0eb637a0c1e0880ce73db99f08f9a451da88
Ju Atanasova <j.atanasova@mail.bg>
2016-03-26 15:41:33 +0000

    initial commit.

---
+++ config.ru
@@ -0,0 +1,2 @@
+require './voter'
+run Sinatra::Application
---
+++ voter.rb
@@ -0,0 +1,47 @@
+require 'sinatra'
+require 'yaml/store'
+
+get '/' do
+  @title = "Добре дошли в машината за гласуване!"
+  erb :index
+end
+
+
+CHOICES = {
+  'happy'        => 'Happy',
+  'divaka'       => 'Дивака',
+  'krivoto'      => 'Кривото',
+  'ugo'          => 'Уго',
+  'mr-pizza'     => 'Мистър Пица',
+  'sun-moon'     => 'Слънце луна',
+  'soul-kitchen' => 'Soul Kitchen',
+}
+
+post '/cast' do
+  @title = 'Благодарим за вашия глас!'
+  @vote  = params['vote']
+  @store = YAML::Store.new 'votes.yml'
+  @store.transaction do
+    if @store['votes'] == nil
+      @store['votes'] = {}
+    end
+
+    if @store['votes'][@vote]
+      @store['votes'][@vote] = @store['votes'][@vote] + 1
+    else
+      @store['votes'][@vote] = 1
+    end
+  end
+  erb :cast
+end
+
+get '/results' do
+  @title = 'Results so far:'
+  @store = YAML::Store.new 'votes.yml'
+  @votes = @store.transaction { @store['votes'] }
+  erb :results
+end
+
+get '/test' do
+  'test routing'
+end
